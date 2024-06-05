7ed13c5df6f1d86228a72a72c4649e0bed14f677
hamed shakib <hamedshakib1@yahoo.com>
2024-06-05 10:41:41 +0330

    Refactor :  read config depend on environment

--- src/Utility/Configuration/Configuration.cs
+++ src/Utility/Configuration/Configuration.cs
@@ -6,9 +6,11 @@ namespace Utility.Configuration
     {
         public static IConfigurationRoot GetAppSettingJson()
         {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCore_Environment");
+
             IConfigurationRoot configuration = new ConfigurationBuilder()
                                         .SetBasePath(Directory.GetCurrentDirectory())
-                                        .AddJsonFile("appsettings.json")
+                                        .AddJsonFile(environment == "Development" ? "appsettings.Development.json" : "appsettings.json")
                                         .Build();
             return configuration;
         }
